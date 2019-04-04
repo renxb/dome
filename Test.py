350e08cb98be62c09ce6b2072de8043f93c6511b
renxb946868 <1663230122@qq.com>
2019-04-04 14:55:08 +0800

    py

--- Test.py
+++ Test.py
@@ -5,4 +5,10 @@ import  xgboost as xgb
 from  pandas import  DataFrame,Series
 data = DataFrame(np.arange(16).reshape(4,4),index=list('abcd'),columns=list('wxyz'))
 print(data)
-
+git add Test.py
+git commit -f "py"
+git remote add origin git@github.com:renxb/dome.git
+git remote add origin git@github.com:xxse/xx.git
+git remote -v
+git remote rm origin
+git pull --rebase origin master
