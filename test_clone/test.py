0b36ce8359330692116e6273ef290b832f17651e
renxb946868 <1663230122@qq.com>
2019-04-04 16:01:56 +0800

    clone

---
+++ test_clone/test.py
@@ -0,0 +1,8 @@
+print("dafsef")
+import  numpy as np
+import  pandas as pd
+import  xgboost as xgb
+from  pandas import  DataFrame,Series
+data = DataFrame(np.arange(16).reshape(4,4),index=list('abcd'),columns=list('wxyz'))
+print(data)
+
